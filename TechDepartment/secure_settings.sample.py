27e74bb0ef88e9e80c66fed9199e5bfe13f3a66c
Hamster Tian <haotia@gmail.com>
2016-09-17 20:27:39 +0800

    Rewrote history to remove database and IDE cond, add sample
    * Do not store DB and IDE things here
    * Add a sample file of secure_settings.py
    * Do not track intermediate static files

---
+++ TechDepartment/secure_settings.sample.py
@@ -0,0 +1,11 @@
+EMAIL_HOST = 'smtp.excited.ha'
+EMAIL_HOST_USER = 'mogician'
+EMAIL_HOST_PASSWORD = '1708'
+EMAIL_PORT = 25
+EMAIL_USE_TLS = False
+
+# Cloud messaging API
+APPKEY = ''
+SECRET = ''
+
+SMS_TEMPLATE_CODE = ''
